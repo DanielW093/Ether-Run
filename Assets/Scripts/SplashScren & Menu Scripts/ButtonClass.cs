77290f8b5e79095a3240842fb43bd7cbec03ad11
Alex Phillips <Alex Phillips>
2016-01-06 23:03:45 +0000

    mybranch

---
+++ Assets/Scripts/SplashScren & Menu Scripts/ButtonClass.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonClass : MonoBehaviour {
+
+    public Canvas CanvasObject;
+    private bool fadeActive = false;
+
+    void Start()
+    {
+        CanvasObject.GetComponent<Canvas>().enabled = false;
+    }
+    void Update()
+    {
+        if(fadeActive == true){
+        StartCoroutine(ChangeLevel());
+        }
+    }
+
+    IEnumerator ChangeLevel()
+    {
+        float fadeTime = GameObject.Find("FadeScreen").GetComponent<Fade>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        Application.LoadLevel("GameScene");
+    }
+
+	public void getButton(string button)
+	{
+		switch (button) 
+		{
+		case "PlayButton": 
+			Debug.Log("playButtonPressed");
+            fadeActive = true;
+			break;
+
+        case "CreditButton":
+            Debug.Log("CreditButtonPressed");
+            fadeActive = false;
+            CanvasObject.GetComponent<Canvas>().enabled = true;
+            break;
+
+
+        case "BackButton":
+            CanvasObject.GetComponent<Canvas>().enabled = false;
+            break;
+		
+
+		}
+	}
+}
