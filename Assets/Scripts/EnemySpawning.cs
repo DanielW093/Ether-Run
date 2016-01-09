2c15592ed1bfa44c4d69b3670bc78456c8b60951
Thomas Bassett <thomasbassett94@gmail.com>
2016-01-09 20:11:41 +0000

    added Ceiling Enemy (take 2)

--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -6,6 +6,7 @@ public class EnemySpawning : MonoBehaviour {
 	//Enemy Variables
 	private GameObject[] enemies = new GameObject[5];
 	public GameObject ethereal;
+    public GameObject etherealCeil;
 
 	private GameObject player;
 
@@ -31,7 +32,19 @@ public class EnemySpawning : MonoBehaviour {
 				spawnX = Random.Range (rightMin, rightMax);
 						
 				Vector3 spawnPos = new Vector3(spawnX, ((int)(player.transform.position.y) + 0.035f), 0f);
-				enemies[i] = (GameObject)Instantiate (ethereal,spawnPos, Quaternion.identity);
+
+                int rand = Random.Range(0, 2);
+                switch (rand)
+                {
+                    case 0:
+                        enemies[i] = (GameObject)Instantiate (ethereal,spawnPos, Quaternion.identity);
+                        break;
+                    case 1:
+                        spawnPos.y = 1.45f;
+                        enemies[i] = (GameObject)Instantiate(etherealCeil, spawnPos, Quaternion.identity);
+                        break;
+                }
+				
 			}
 		}
 	}
